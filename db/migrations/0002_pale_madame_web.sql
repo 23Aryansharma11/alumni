40c1ca660b3dae33f993ce22338f0568e84ad2c8
23Aryansharma11 <23aryansharma11@gmail.com>
2025-09-22 10:42:37 +0530

    remove scrollbar

---
+++ db/migrations/0002_pale_madame_web.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "posts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
